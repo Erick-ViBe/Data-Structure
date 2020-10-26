6507bc7dd32460e722bca1328fb550165368692b
Erick-ViBe <erickvb12@gmail.com>
2020-10-26 16:10:35 -0600

    Complete linked list code

--- LinkedList/LinkedList.c
+++ LinkedList/LinkedList.c
@@ -3,30 +3,38 @@
 #include <stdbool.h>
 
 struct node;
+
 bool is_empty(struct node *n);
 void insert_at_start(struct node **n, int data);
-void delete_data(struct node *n, int data_to_delete);
+void insert_at_the_end(struct node *n, int data);
 void delete_front(struct node **n);
+void delete_end(struct node **n);
+void delete_data(struct node **n, int data_to_delete);
 void show_linked_list(struct node *n);
+void reverse_show_linked_list(struct node *n);
 int length_listed_list(struct node *n);
 
 int main(void){
     struct node* linked_list=NULL;
 
-    insert_at_start(&linked_list, 5);
     insert_at_start(&linked_list, 4);
     insert_at_start(&linked_list, 3);
     insert_at_start(&linked_list, 2);
     insert_at_start(&linked_list, 1);
 
+    insert_at_the_end(linked_list, 5);
+
     delete_front(&linked_list);
-    delete_data(linked_list, 5);
+
+    delete_data(&linked_list, 3);
+
+    delete_end(&linked_list);
 
     show_linked_list(linked_list);
 
-    delete_data(linked_list, 5);
-
-    show_linked_list(linked_list);
+    printf("-\n");
+    reverse_show_linked_list(linked_list);
+    printf("-\n");
 
     printf("Linked List Length: %d\n", length_listed_list(linked_list));
 
@@ -51,6 +59,19 @@ void insert_at_start(struct node **n, int data){
     *n = new;
 }
 
+void insert_at_the_end(struct node *n, int data){
+    struct node* previous;
+    struct node* new = malloc(sizeof(struct node));
+    new->data = data;
+
+    while(n != NULL){
+        previous = n;
+        n = n->next;
+    }
+
+    previous->next = new;
+}
+
 void delete_front(struct node **n){
     struct node* deleted_node = *n;
     if(!is_empty(*n)){
@@ -61,24 +82,51 @@ void delete_front(struct node **n){
     }
 }
 
-void delete_data(struct node *n, int data_to_delete){
-    struct node* previous;
-    struct node* linked_list = n;
-    
-    while(n->data != data_to_delete && n->next != NULL){
-        previous = n;
-        n = n->next;
-    }
-
-    if(n->data == data_to_delete){
-        if(linked_list == n){
-            *n = *n->next;
+void delete_end(struct node **n){
+    if(!is_empty(*n)){
+        if((*n)->next == NULL){
+            delete_front(n);
         }else{
-            previous->next = n->next;
-            free(n);
+            struct node* previous;
+            struct node* linked_list = *n;
+
+            while(linked_list->next != NULL){
+                previous = linked_list;
+                linked_list = linked_list->next;
+            }
+
+            previous->next = NULL;
+
+            free(linked_list);
         }
     }else{
-        printf("The data provided not already exist in the linked list\n");
+        printf("The Linked List is already empty\n");
+    }
+}
+
+void delete_data(struct node **n, int data_to_delete){
+    if(!is_empty(*n)){
+        struct node* previous;
+        struct node* linked_list = *n;
+
+        if((*n)->data == data_to_delete){
+            delete_front(n);
+        }else{
+            while(linked_list->data != data_to_delete && linked_list->next != NULL){
+                previous = linked_list;
+                linked_list = linked_list->next;
+            }
+
+            if(linked_list->data == data_to_delete){
+                previous->next = linked_list->next;
+
+                free(linked_list);
+            }else{
+                printf("The data provided not already exist in the linked list\n");
+            }
+        }
+    }else{
+        printf("The Linked List is already empty\n");
     }
 }
 
@@ -90,6 +138,15 @@ void show_linked_list(struct node *n){
         n = n->next;
     }
     printf("-\n");
+}
+
+void reverse_show_linked_list(struct node *n){
+    if(n->next != NULL){
+        reverse_show_linked_list(n->next);
+        printf("%d\n", n->data);
+    }else{
+        printf("%d\n", n->data);
+    }
 }
 
 int length_listed_list(struct node *n){
