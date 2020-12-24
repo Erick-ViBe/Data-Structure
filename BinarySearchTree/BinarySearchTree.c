2c4402e8b651a0576bcb4ad159d584faa52cabe4
Erick-ViBe <erickvb12@gmail.com>
2020-12-24 14:17:42 -0600

    Push something

--- BinarySearchTree/BinarySearchTree.c
+++ BinarySearchTree/BinarySearchTree.c
@@ -1,10 +1,14 @@
+#include <cstddef>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 struct node;
 
 void insert(struct node **root, int data);
-void insert(struct node **root, int data);
+bool elementIn(struct node *root, int data_to_search);
+/*void delete(struct node **root, int data_to_delete);*/
 void show(struct node *root);
 
 int main(void){
@@ -21,6 +25,14 @@ int main(void){
 	insert(&bst, 1);
 	insert(&bst, 3);
 	insert(&bst, 9);
+
+	printf("+\n");
+	printf("%d\n", elementIn(bst, 5));
+	printf("+\n");
+
+	printf("+\n");
+	printf("%d\n", elementIn(bst, 111));
+	printf("+\n");
 
 	printf("-\n");
 	show(bst);
@@ -51,6 +63,52 @@ void insert(struct node **root, int data){
 				insert(&(*root)->rigth_child, data);
 			}else{
 				insert(&(*root)->left_child, data);
+			}
+		}
+	}
+}
+
+bool elementIn(struct node *root, int data_to_search){
+	if(root == NULL){
+		return false;
+	}else if(root->data == data_to_search){
+		return true;
+	}else if(data_to_search < root->data){
+		return elementIn(root->left_child, data_to_search);
+	}else{
+		return elementIn(root->rigth_child, data_to_search);
+	}
+}
+
+void delete(struct node **root, int data_to_delete){
+	struct node *bst = *root;
+	struct node *previous = NULL;
+	struct node *tmp = NULL;
+
+	int data_temp;
+	
+	while(bst != NULL){
+		if(bst->data == data_to_delete){
+			if(previous == NULL){
+				if(bst->rigth_child != NULL){
+					if(bst->rigth_child->left_child != NULL){
+						data_temp = bst->rigth_child->left_child->data;
+					}
+					bst->rigth_child->left_child = bst->left_child;
+					*root = bst->rigth_child;
+					if(tmp != NULL){
+						insert(root, data_temp);
+					}
+					free(bst);
+				}else if(bst->left_child != NULL){
+					tmp = bst;
+					*root = bst->left_child;
+					free(tmp);
+				}else{
+					tmp = bst;
+					*root = NULL;
+					free(tmp);
+				}
 			}
 		}
 	}
